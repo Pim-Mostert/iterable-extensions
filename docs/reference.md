15d17ecbfb24067e68ce1af3ea45d991516f5806
Pim Mostert <pim.mostert@pimmostert.com>
2026-02-17 14:28:10 +0100

    Add MkDocs and ReadTheDocs

---
+++ docs/reference.md
@@ -0,0 +1,10 @@
+# Reference
+
+::: iterable_extensions.count
+::: iterable_extensions.group_by
+::: iterable_extensions.order_by
+::: iterable_extensions.order_by_descending
+::: iterable_extensions.select
+::: iterable_extensions.to_dictionary
+::: iterable_extensions.to_list
+::: iterable_extensions.where
--- src/iterable_extensions/__init__.py
+++ src/iterable_extensions/__init__.py
@@ -1,6 +1,15 @@
 from importlib.metadata import PackageNotFoundError, version
 
-from .iterable_extensions import where
+from .iterable_extensions import (
+    count,
+    group_by,
+    order_by,
+    order_by_descending,
+    select,
+    to_dictionary,
+    to_list,
+    where,
+)
 
 try:
     __version__ = version("iterable_extensions")
@@ -9,4 +18,11 @@ except PackageNotFoundError:
 
 __all__ = [
     "where",
+    "select",
+    "to_list",
+    "to_dictionary",
+    "order_by",
+    "order_by_descending",
+    "group_by",
+    "count",
 ]
